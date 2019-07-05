1ebe8a2b25e57b20750678e182c276a37d73a48f
付义兵 <websearch@163.com>
2019-07-05 15:57:29 +0800

    no message

--- src/Base/annotation.php
+++ src/Base/annotation.php
@@ -1,10 +1,40 @@
 <?php
+
 /**
  * 解析SDK的ID注解
  * @author wsfuyibing <websearch@163.com>
  * @date   2019-07-05
  */
+class SdkAnnotation
+{
+    private $path;
+    private $namespaces = [
+        'mbs' => Uniondrug\ServiceSdk\Compatiables\Abstracts\Sdk::class
+    ];
 
+    public function __construct()
+    {
+        $this->path = __DIR__.'/..';
+    }
 
+    public function scanner()
+    {
+        $this->scanCompatiables();
+        $this->scanExports();
+    }
 
+    /**
+     * 扫描兼容目录
+     * SDK/1.x
+     */
+    public function scanCompatiables()
+    {
+    }
+
+    public function scanExports()
+    {
+    }
+}
+
+(new SdkAnnotation())->scanner();
 
